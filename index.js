fa5cdb916e9c6d553980a09eab8058933864f71a
cadujvieira <cadu.jvieira@gmail.com>
2026-05-12 02:48:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1364,7 +1364,6 @@ app.get("/dashboard-audience", async (req, res) => {
 <td>${u.campanhaOrigem}</td>
 <td>${u.campanhas.join("<br>")}</td>
 <td>${u.depositos}</td>
-<td>${u.ftd}</td>
 <td>R$ ${u.receita.toFixed(2)}</td>
 <td>${u.score || 0}</td>
 <td><span class="${u.nivelScore || ""}">${u.nivelScore || "-"}</span></td>
@@ -1433,7 +1432,6 @@ app.get("/dashboard-audience", async (req, res) => {
   <th>Origem <span class="info">?<span class="tooltip">Origem principal identificada do usuário através dos parâmetros UTM.</span></span></th>
   <th>Campanhas tocadas <span class="info">?<span class="tooltip">Lista de campanhas que tiveram interação com este usuário.</span></span></th>
   <th>Depósitos <span class="info">?<span class="tooltip">Quantidade total de depósitos realizados.</span></span></th>
-  <th>FTD <span class="info">?<span class="tooltip">First Time Deposit do usuário.</span></span></th>
   <th>Receita <span class="info">?<span class="tooltip">Receita total gerada pelo usuário.</span></span></th>
   <th>Score <span class="info">?<span class="tooltip">Pontuação comportamental baseada em depósitos, frequência e receita.</span></span></th>
   <th>Nível <span class="info">?<span class="tooltip">Classificação avançada baseada no score final.</span></span></th>
